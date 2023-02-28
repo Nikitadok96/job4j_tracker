0f29e179419bb29c8ca61b27aa31d87915fe7651
Mingrow <nikitosvozh@gmail.com>
2023-02-28 20:10:06 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #395852]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,12 +57,12 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        item.setId(id);
         if (index != -1) {
+            item.setId(id);
             items[index] = item;
             return true;
-        } else {
-            return false;
         }
+        return false;
+
     }
 }
