224dd8ab7977954c0f1ea6acbf064b4e8b8b6156
Mingrow <nikitosvozh@gmail.com>
2023-03-01 21:26:29 +0300

    4.2. Интерфейс Input [#181092]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,20 +1,4 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport {
-
-    @Override
-    public void drive() {
-        System.out.println("Автобус едет");
-    }
-
-    @Override
-    public void passengers(int count) {
-        System.out.println("Автобус внестил " + count);
-    }
-
-    @Override
-    public double refuel(int count) {
-        return count * 50;
-    }
-
 }
--- src/main/java/ru/job4j/poly/Transport.java
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -2,9 +2,6 @@ package ru.job4j.poly;
 
 public interface Transport {
 
-    void drive();
 
-    void passengers(int count);
 
-    double refuel(int count);
 }
