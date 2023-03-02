1355e3e4ea594c42f125bfa02efc6e7f9e64d273
Mingrow <nikitosvozh@gmail.com>
2023-03-02 20:36:27 +0300

    8. Input и полиморфизм. [#181094 #397049]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -6,7 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 class StartUITest {
     @Test
     public void whenAddItem() {
-        Input input = new ConsoleInput();
+        String[] answers = {"Fix PC"};
+        Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
         StartUI.createItem(input, tracker);
         Item created = tracker.findAll()[0];
