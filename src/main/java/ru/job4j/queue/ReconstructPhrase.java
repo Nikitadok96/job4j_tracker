15e5d0680da17eba6001efc07e10a240401e231a
Mingrow <nikitosvozh@gmail.com>
2023-03-15 21:06:16 +0300

    2. Интерфейс Deque [#504943]

---
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -0,0 +1,38 @@
+package ru.job4j.queue;
+
+import java.util.Deque;
+
+public class ReconstructPhrase {
+    private final Deque<Character> descendingElements;
+
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < evenElements.size() + i; i++) {
+            if (i % 2 == 0) {
+                stringBuilder.append(evenElements.poll());
+            } else {
+                evenElements.poll();
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private String getDescendingElements() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < descendingElements.size() + i; i++) {
+            stringBuilder.append(descendingElements.pollLast());
+        }
+        return stringBuilder.toString();
+    }
+
+    public String getReconstructPhrase() {
+        return getEvenElements() + getDescendingElements();
+    }
+}
