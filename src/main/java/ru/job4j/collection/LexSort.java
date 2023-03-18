2009a30b3478a3e108c40b3dfbea02e2b3d0c48d
Mingrow <nikitosvozh@gmail.com>
2023-03-18 12:30:29 +0300

    4. Сортировка номера [#221284 #401975]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -5,18 +5,8 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        String stringsLeft = left.split("\\.")[0];
-        String stringsRight = right.split("\\.")[0];
-        int range = Math.min(stringsLeft.length(), stringsRight.length());
-        for (int i = 0; i < range; i++) {
-            if (Character.isDigit(stringsLeft.charAt(i)) && Character.isDigit(stringsRight.charAt(i))) {
-                int leftNum = Integer.parseInt(stringsLeft);
-                int rightNum = Integer.parseInt(stringsRight);
-                if (leftNum != rightNum) {
-                    return Integer.compare(leftNum, rightNum);
-                }
-            }
-        }
-        return Integer.compare(stringsLeft.length(), stringsRight.length());
+        int numLeft = Integer.parseInt(left.split("\\.")[0]);
+        int numRight = Integer.parseInt(right.split("\\.")[0]);
+        return Integer.compare(numLeft, numRight);
     }
 }
