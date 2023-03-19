cccd93748df7dae2f1de9368caf7071a165a2e03
Mingrow <nikitosvozh@gmail.com>
2023-03-19 13:11:55 +0300

    3. Отсортировать департаменты [#34352 #402148]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,17 +8,7 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String left, String right) {
         String[] leftArray = left.split("/");
         String[] rightArray = right.split("/");
-        if (rightArray[0].compareTo(leftArray[0]) != 0) {
-            return rightArray[0].compareTo(leftArray[0]);
-        } else {
-            int range = Math.min(leftArray.length, rightArray.length);
-            for (int i = 1; i < range; i++) {
-                int rsl = leftArray[i].compareTo(rightArray[i]);
-                if (rsl != 0) {
-                    return rsl;
-                }
-            }
-        }
-        return Integer.compare(leftArray.length, rightArray.length);
+        int rsl = rightArray[0].compareTo(leftArray[0]);
+        return rsl != 0 ? rsl : left.compareTo(right);
     }
 }
