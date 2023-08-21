457e5f4e2663ac4f8a7c398b7bf27c7815077092
Mingrow <nikitosvozh@gmail.com>
2023-08-21 07:21:01 +0300

    1. Профилирование. [#1591]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -34,10 +34,11 @@ public class StartUI {
     public static void main(String[] args) {
         Output output = new ConsoleOutput();
         Input input = new ValidateInput(output, new ConsoleInput());
-        Store tracker = new SqlTracker();
-        List<UserAction> actions = Arrays.asList(new CreateAction(output), new ShowAction(output),
-                new ReplaceAction(output), new DeleteAction(output), new FindByIdAction(output),
-                new FindByNameAction(output), new Exit(output));
+        Store tracker = new MemTracker();
+        List<UserAction> actions = Arrays.asList(new CreateAction(output), new CreateManyItems(output),
+                new ShowAction(output), new ReplaceAction(output),
+                new DeleteAction(output), new DeleteAllItems(output),
+                new FindByIdAction(output), new FindByNameAction(output), new Exit(output));
         new StartUI(output).init(input, tracker, actions);
     }
 
